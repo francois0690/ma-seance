8ccaaad8dd117b98992543a5deae3967b8c42986
Francois Jouvray <francois0690@gmail.com>
2020-06-13 11:29:11 +0200

    migration user et speciality

--- db/migrate/20200610080352_create_specialities.rb
+++ db/migrate/20200610080352_create_specialities.rb
@@ -2,6 +2,7 @@ class CreateSpecialities < ActiveRecord::Migration[6.0]
   def change
     create_table :specialities do |t|
       t.string :name
+      t.string :description
 
       t.timestamps
     end
