4016442386b4c6901dd4586f8abb6860165f2b4e
Francois Jouvray <francois0690@gmail.com>
2020-06-16 21:36:05 +0200

    seed ok

--- db/migrate/20200610081427_create_offices.rb
+++ db/migrate/20200610081427_create_offices.rb
@@ -2,9 +2,7 @@ class CreateOffices < ActiveRecord::Migration[6.0]
   def change
     create_table :offices do |t|
       t.string :name
-      t.string :street
-      t.string :zip_code
-      t.string :city
+      t.string :address
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -67,9 +67,7 @@ ActiveRecord::Schema.define(version: 2020_06_13_121629) do
 
   create_table "offices", force: :cascade do |t|
     t.string "name"
-    t.string "street"
-    t.string "zip_code"
-    t.string "city"
+    t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
