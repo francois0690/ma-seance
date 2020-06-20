107ca7a1d6043f2a9f0271446742bf871849ad02
Francois Jouvray <francois0690@gmail.com>
2020-06-20 11:00:50 +0200

    fix heroku 2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,6 +34,6 @@ import { initMapbox } from '../plugins/init_mapbox';
 document.addEventListener('turbolinks:load', () => {
     // Call your functions here, e.g:
     // initSelect2();
-    bubler
+    bubller
     initMapbox();
 });
