0725a4b8dedcab5318e48231b7017a83b37b437d
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-06-23 21:35:15 +0200

    back du show du medecin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def dashboard
   end
+
+  def show
+    set_user
+    @jobs = @user.jobs
+    @activities = @user.activities
+    # @offices = activities.offices
+    # @specialities = activities.specialities
+    # @consultation = activities.onsultation
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def piece_params
+    params.require(:user).permit(:id)
+  end
 end
