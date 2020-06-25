23f11f758c654633836933f689f34c2f83ff525a
Francois Jouvray <francois0690@gmail.com>
2020-06-25 12:20:26 +0200

    showmore v2

--- app/javascript/plugins/init_readmore.js
+++ app/javascript/plugins/init_readmore.js
@@ -7,7 +7,7 @@ const initReadMore = () => {
     let speciliies = document.querySelectorAll(".speciality > p");
     $(".speciality > p").each(function(e) {
         $(this).readmore({
-            speed: 75,
+            speed: 150,
             collapsedHeight: box_sizes[Math.floor(Math.random() * 12)],
             moreLink: '<a href="#">lire la suite</a>',
             lessLink: '<a href="#">réduire</a>'
@@ -18,18 +18,61 @@ const initReadMore = () => {
 const initReadMoreT = () => {
 
     // page des toubibs
-    const doctorsDesc = document.querySelectorAll(".info #description");
-
-
+    // const doctorsDesc = document.querySelectorAll(".info .description");
+    // console.log(doctorsDesc);
 
     // console.log (doctorsDesc(0).innerHTML);
-    $(".info #description").each(function(e) {
-        $(this).readmore({
-          speed: 75,
-            moreLink: '<a href="#">Lire la suite...</a>',
-            lessLink: '<a href="#">Réduire</a>'
-        });
-    });
+
+
+
+    // $(".info #description").readmore({
+    //         speed: 75,
+    //         moreLink: '<a href="#">Lire la suite...</a>',
+    //         lessLink: '<a href="#">Réduire</a>'
+
+
+    //         // afterToggle: function(trigger, element, expanded) {
+    //         //   if(! expanded) { console.log("toto") }
+
+
+    // });
+
+
+    // jQuery(document).ready(function ($) {
+    //   $('.info #description').readmore({
+
+$(".info #description").readmore({
+            speed: 550,
+            moreLink: '<a class="moreLink" href="#">Lire la suite...</a>',
+            lessLink: '<a class="lessLink" href="#">Réduire</a>',
+            // afterToggle: function() { console.log("afterToogle") } ,
+            beforeToggle: function() { console.log("beforeToogle") },
+
+// if(! expanded) { console.log("toto") }}
+
+          });
+
+
+
+
+    // const links = document.querySelectorAll(".info > a");
+    // links.forEach((link) => {
+
+    //   console.log(link.innerHTML);
+    //   if (link.innerHTML == "Lire la suite...") {
+    //     console.log(link.innerHTML);
+    //   }
+
+    // });
+
+
+// document.querySelectorAll("img").forEach((img) => {
+//   img.addEventListener("click", (event) => {
+//     event.currentTarget.classList.toggle("img-circle");
+//   });
+// });
+
+
 };
 
 export { initReadMore, initReadMoreT };
