aea931e5d9d885e42a216b3ca41575d0495b4321
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-07-04 16:53:58 +0200

    loadbar ok

--- app/javascript/plugins/init_loadingbar.js
+++ app/javascript/plugins/init_loadingbar.js
@@ -1,5 +1,10 @@
-import loading-bar.js from 'loading-bar';
+import ldBar from 'loadingio-bar';
 
-const loading = () => {};
+const loading = () => {
+  const bar = new ldBar(".ldBar")
+  bar.set(87);
+  const bar2 = new ldBar(".ldBar2")
+  bar2.set(72);
+};
 
 export { loading };
