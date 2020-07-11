92527cf5ed2ea500ed096dbe46409dd00be5be37
Myriam de Bossoreille <myriam.dbdr@gmail.com>
2020-07-11 14:55:01 +0200

    token

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,17 +3,7 @@ class UserMailer < ApplicationMailer
   def aubergine_email()
     @user = params[:user]
     @room = params[:room]
-    @token = Devise.friendly_token.first(16)
-    @user.auth_token = @token
     mail(to: @user.email, subject: "Nouveau contact, Ma-séance")
-  end
-
-
-  def self.generate(email)
-    user = User.find_by(email: email)
-    return nil if !user
-
-    create(user: user, expires_at: Date.today + 1.day, token: generate_token)
   end
 
 end
